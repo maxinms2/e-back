f675b5b418aef534f6038fd7e82e35555bf25822
Edgar Mejia <you@example.com>
2024-06-25 20:05:50 -0600

    Administración de productos

--- src/main/java/com/icodeap/ecommerce/backend/infrastructure/rest/ProductController.java
+++ src/main/java/com/icodeap/ecommerce/backend/infrastructure/rest/ProductController.java
@@ -4,6 +4,9 @@ import com.icodeap.ecommerce.backend.application.ProductService;
 import com.icodeap.ecommerce.backend.domain.model.Product;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import java.math.BigDecimal;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,7 +21,24 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping
-    public ResponseEntity<Product> save(@RequestBody Product product){
+    public ResponseEntity<Product> save(@RequestParam("id") Integer id,
+    		@RequestParam("code") String code,
+            @RequestParam("name") String name,
+            @RequestParam("description") String  description,
+            @RequestParam("price")BigDecimal price,
+            @RequestParam("urlImage")String urlImage,
+            @RequestParam("userId")Integer userId,
+            @RequestParam("categoryId")Integer categoryId){
+    	
+        Product product = new Product();
+        product.setId(id);
+        product.setCode(code);
+        product.setName(name);
+        product.setDescription(description);
+        product.setPrice(price);
+        product.setCategoryId(categoryId);
+        product.setUserId(userId);
+        product.setUrlImage(urlImage);
         log.info("NOmbre producto: {}", product.getName());
         return  new ResponseEntity<>(productService.save(product), HttpStatus.CREATED);
     }
