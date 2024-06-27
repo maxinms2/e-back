7abe7e7f0373210f560a755d21ab1a5700cb4cbb
Edgar Mejia <you@example.com>
2024-06-27 09:38:44 -0600

    Cross origin categorias

--- src/main/java/com/icodeap/ecommerce/backend/infrastructure/rest/CategoryController.java
+++ src/main/java/com/icodeap/ecommerce/backend/infrastructure/rest/CategoryController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/admin/categories")
+@CrossOrigin(origins="http://localhost:4200")
 @Slf4j
 public class CategoryController {
     private final CategoryService categoryService;
