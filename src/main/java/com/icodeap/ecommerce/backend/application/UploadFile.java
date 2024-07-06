c6c9e381405212b843a5988c01f9264771b5fa8e
Edgar Mejia <you@example.com>
2024-07-06 09:53:08 -0600

    Rollbac2

--- src/main/java/com/icodeap/ecommerce/backend/application/UploadFile.java
+++ src/main/java/com/icodeap/ecommerce/backend/application/UploadFile.java
@@ -3,6 +3,7 @@ package com.icodeap.ecommerce.backend.application;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.icodeap.ecommerce.backend.infrastructure.entity.ParameterEntity;
 import com.icodeap.ecommerce.backend.infrastructure.service.ParameterService;
 
 import java.io.File;
@@ -13,18 +14,22 @@ import java.nio.file.Paths;
 
 public class UploadFile {
     //private final String FOLDER = "src//main//resources//static//images//";
-    private final String FOLDER = "C:/files/images/";
+    private final String FOLDER = System.getProperty("user.home")+"/files/images/";
     private final String IMG_DEFAULT = "default.jpg";
-    private final String URL = "http://localhost:8085/images/";
+    private String URL=""; //= "http://localhost:8085/images/";
     
     private final ParameterService parameterService;
     
     
 
     public UploadFile(ParameterService parameterService) {
-		this.parameterService = parameterService;
+		this.parameterService = parameterService;    	
+		//ParameterEntity parameter=parameterService.getParameter("connection.urlback").get();
+    	//URL=parameter.getValor()+"/images/";
 	}
 	public String upload (MultipartFile multipartFile) throws IOException {
+		ParameterEntity parameter=parameterService.getParameter("connection.urlback").get();
+    	URL=parameter.getValor()+"/images/";
         if (multipartFile!=null){
             byte [] bytes = multipartFile.getBytes();
             Path path = Paths.get(FOLDER+multipartFile.getOriginalFilename());
